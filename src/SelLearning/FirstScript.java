089efb55b28f76d7c1cfe6b37337cdfcd91b9ee4
I543465 <I543465@W-8LVJ593>
2024-02-14 21:32:25 +0530

    First Script Updated to google.com

--- src/SelLearning/FirstScript.java
+++ src/SelLearning/FirstScript.java
@@ -6,7 +6,7 @@ public class FirstScript {
 	public static void main(String[] args) {
 	    System.setProperty("webdriver.chrome.driver", "C:\\Users\\I543465\\OneDrive - SAP SE\\Pers\\Selenium\\chromedriver-win64\\chromedriver.exe");  
 	    WebDriver driver = new ChromeDriver(); 
-	    driver.get("https://amazon.com");
+	    driver.get("https://google.com");
 
 	}
 
